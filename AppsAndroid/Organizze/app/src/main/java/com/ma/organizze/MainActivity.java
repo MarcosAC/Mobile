3f05f4fd9d97bd3f1e37dd00c44a7fbf543f1858
Marcos Aurelio <marcos.aucorrea@gmail.com>
2025-02-11 22:13:49 -0300

    Botao para criar conta e nevegacao para a tela de cadastro

--- AppsAndroid/Organizze/app/src/main/java/com/ma/organizze/MainActivity.java
+++ AppsAndroid/Organizze/app/src/main/java/com/ma/organizze/MainActivity.java
@@ -1,9 +1,12 @@
 package com.ma.organizze;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -31,6 +34,16 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        TextView tvCriarConta =findViewById(R.id.tvCriarConta);
+
+        tvCriarConta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new
+                startActivity(new Intent(MainActivity.this, CadastroActivity.class));
+            }
         });
     }
 }
