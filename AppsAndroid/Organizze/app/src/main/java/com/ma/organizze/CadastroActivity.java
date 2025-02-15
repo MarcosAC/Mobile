15eb16a3cbc16953a8994df2eb5d81a41f0898f7
Marcos Aurelio <marcos.aucorrea@gmail.com>
2025-02-15 12:02:45 -0300

    Botao voltar na tela de cadastro

--- AppsAndroid/Organizze/app/src/main/java/com/ma/organizze/CadastroActivity.java
+++ AppsAndroid/Organizze/app/src/main/java/com/ma/organizze/CadastroActivity.java
@@ -1,9 +1,14 @@
 package com.ma.organizze;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,11 +18,28 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class CadastroActivity extends AppCompatActivity {
+    private EditText etNome, etEmail, etSenha;
+    private Button btCadastrar;
+    private ImageButton btVoltar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_cadastro);
+
+        etNome = findViewById(R.id.etNome);
+        etEmail = findViewById(R.id.etEmailCadastro);
+        etSenha = findViewById(R.id.etSenhaCadastro);
+
+        btVoltar = findViewById(R.id.btVoltar);
+        btCadastrar = findViewById(R.id.btCadastrar);
+
+        btVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(CadastroActivity.this, MainActivity.class));
+            }
+        });
     }
 }
