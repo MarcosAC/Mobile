7586e4b55006af8f462dcf1efe2aa1bb4fd86d64
Marcos Aurelio <marcos.aucorrea@gmail.com>
2025-02-16 20:44:54 -0300

    Models movimentacao, usuario e helper base64

---
+++ AppsAndroid/Organizze/app/src/main/java/model/Movimentacao.java
@@ -0,0 +1,82 @@
+package model;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Base64;
+
+import fireBaseService.ConfiguracaoFirebase;
+import helper.Base64Custom;
+
+public class Movimentacao {
+    private String data;
+    private String categoria;
+    private String descricao;
+    private String tipo;
+    private Double valor;
+    private String key;
+
+    public Movimentacao() {}
+
+    public void salvar(String dataEscolha) {
+        FirebaseAuth autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+        String idUsuario = Base64Custom.codificarBase64(autenticacao.getCurrentUser().getEmail());
+        //String mesAno = DateCustom.mesAnoDataEscolhida(dataEscolhida);
+
+        DatabaseReference firebase =ConfiguracaoFirebase.getFirebaseDatabase();
+        firebase.child("movimentacao")
+                .child(idUsuario)
+                .push()
+                .setValue(this);
+
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
